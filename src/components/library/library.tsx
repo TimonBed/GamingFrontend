86a411e3b66eb02e70cc5a7cfbda5cedc7f5d35f
Timon Laptop <bedynek@themarmalade.com>
2024-03-12 15:45:44 +0100

    fix

--- src/components/library/library.tsx
+++ src/components/library/library.tsx
@@ -63,7 +63,7 @@ const Library = () => {
   return (
     <div className="bg-gray-900 text-brandtext">
       <div className="max-w-[2100px] mx-auto items-center">
-        <div className="mt-32 mx-8 max-w-[1500px] sm:mx-auto items-center flex flex-col">
+        <div className="pt-32 mx-8 max-w-[1500px] sm:mx-auto items-center flex flex-col">
           <h1 className="text-4xl font-bold tracking-tight">
             Game Mechanics Reference Library
           </h1>
