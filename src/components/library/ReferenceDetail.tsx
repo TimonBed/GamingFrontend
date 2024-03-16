90cc287491db4160245f8553b5fb956059bec6a9
TimonPC\Timon <gtreet>
2024-03-16 19:12:31 +0100

    fixed height

--- src/components/library/ReferenceDetail.tsx
+++ src/components/library/ReferenceDetail.tsx
@@ -82,7 +82,7 @@ const ReferenceDetail = () => {
 
   const [isMaximized, setIsMaximized] = useState(false);
   return (
-    <div className="pt-32 text-brandtext bg-slate-700">
+    <div className="pt-32 text-brandtext bg-slate-700 h-full">
       {/* maximized window popup */}
       {isMaximized ? (
         <div className="fixed inset-0 z-50 bg-black bg-opacity-70">
