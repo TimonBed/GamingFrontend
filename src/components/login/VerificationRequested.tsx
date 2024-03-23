11c3d9444ec347e1526af4561d51eee1b006f1e4
TimonPC\Timon <gtreet>
2024-03-23 21:45:02 +0100

    fix

--- src/components/login/VerificationRequested.tsx
+++ src/components/login/VerificationRequested.tsx
@@ -5,9 +5,11 @@ const VerificationRequested = () => {
   const { user } = useUser();
   const handleResendEmail = () => {
     // resend email
-    axios.post(`${import.meta.env.VITE_BACKEND_URL}/auth/resend-email/`, {
-      email: user?.email,
-    });
+    if (user && user.email) {
+      axios.post(`${import.meta.env.VITE_BACKEND_URL}/auth/resend-email/`, {
+        email: user.email,
+      });
+    }
   };
 
   return (
